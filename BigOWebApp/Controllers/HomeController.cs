e0c00f001d9b631911f50adec95e62a693db4193
Zafer CÖMERT <zafer.comert@hotmail.com>
2022-04-10 22:35:16 +0300

    BigO Notation Web Application has been added to the project.

---
+++ BigOWebApp/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using BigOWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace BigOWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult SortingAlgorithms()
+        {
+            return View();
+        }
+
+        public IActionResult CommonDataStructureOperations()
+        {
+            return View();
+        }
+    }
+}
