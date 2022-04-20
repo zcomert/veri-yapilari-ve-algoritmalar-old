8ac40f35c2ff058a593605c76366601a0784db41
zcomert <zafer.comert@hotmail.com>
2022-04-20 21:02:12 +0300

    Recursion has been implemented.

---
+++ Math/Math.cs
@@ -0,0 +1,32 @@
+﻿namespace Recursion
+{
+    public class Math
+    {
+        // Math.Factorial(int)
+        // Iteration
+        public static int Factorial(int input)
+        {
+            if (input <= 1)
+                return 1;
+
+            int result = 1;
+            int step = 1;
+            while (step <= input)
+            {
+                result *= step;
+                step++;
+            }
+            return result;
+        }
+
+
+        // Recursion
+        public static int FactorialRecursion(int input)
+        {
+            // base case
+            if (input <= 1)
+                return 1;
+            return input * FactorialRecursion(input - 1);
+        }
+    }
+}
---
+++ RecursionFactorialApp/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Bir sayi giriniz: ");
+
+int input = int.Parse(Console.ReadLine());
+var result = Recursion.Math.Factorial(input);
+
+Console.WriteLine($"{input}! = {result}");
