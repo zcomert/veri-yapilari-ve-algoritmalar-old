0efbcacd392f886094a9f1b823882cc1015ca5ae
zcomert <zafer.comert@hotmail.com>
2022-04-26 17:40:19 +0300

    Binary Search, Divide and Conquer, Recursion Substitution Method have been implemented.

---
+++ Search/BinarySearch.cs
@@ -0,0 +1,35 @@
+﻿namespace Search
+{
+    public class BinarySearch
+    {
+        public static int Search<T>(T[] input, T key) where T : IComparable<T>
+        {
+            return search(input, 0, input.Length - 1, key);
+        }
+
+        private static int search<T>(T[] input, int i, int j, T key) where T : IComparable<T>
+        {
+            while (true)
+            {
+                if (i == j)
+                {
+                    if (input[i].Equals(key))
+                        return i;
+                    return -1;
+                }
+
+                int mid = (i + j) / 2;
+
+                if (input[mid].Equals(key))
+                    return mid;
+
+                if (input[mid].CompareTo(key) >= 1)
+                {
+                    j = mid;
+                    continue;
+                }
+                i = mid + 1;
+            }
+        }
+    }
+}
---
+++ SearchApp/Program.cs
@@ -0,0 +1,5 @@
+﻿var arr = new int[] { 10, 20, 30, 40, 50, 60, 70, 80 };
+
+var index = Search.BinarySearch.Search<int>(arr, 20);
+
+Console.WriteLine($"arr[{index}] = {arr[index]}");
